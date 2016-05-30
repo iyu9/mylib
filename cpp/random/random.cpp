48732acf3f64ac56c1f74feac9f4153c33cfa2ab
Tomii Yuto <tomii.yuto@gmail.com>
2016-05-30 23:11:44 +0900

    rand fix

--- cpp/random/random.cpp
+++ cpp/random/random.cpp
@@ -18,10 +18,14 @@ int ran::max() {
 	return RAND_MAX;		
 }
 
+float ran::per() {
+	return ((float)rand() / (float)RAND_MAX);
+}
 
 int main() {
 	ran* r;
 	std::cout << r->max() << std::endl;
 	std::cout << r->value() << std::endl;
+	std::cout << r->per() << std::endl;
 	return 0;
 }
--- cpp/random/random.h
+++ cpp/random/random.h
@@ -1,14 +1,12 @@
-#include "tinymt32.c"
 #include "../common/common.h"
 
 class ran {
 
 public:
-	//constructors
 	ran();
 	~ran();
 
-	//functions
 	int max();
 	int value();
+	float per();
 };
