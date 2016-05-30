affd33e6d55097fe444247bb8312d51db70e3a40
Tomii Yuto <tomii.yuto@gmail.com>
2016-05-31 00:31:22 +0900

    card

---
+++ cpp/card/card.cpp
@@ -0,0 +1,39 @@
+#include "card.h"
+
+card::card() {
+	this->id = 0;
+	this->suit = CLUB;
+	this->num = 0;
+	this->suit_str = suit_name[CLUB];
+}
+
+card::card(int id) {
+	this->id = id;
+	this->suit = id % 4;
+	this->num = id / 4;
+	this->suit_str = suit_name[this->suit];
+}
+
+card::card(int suit, int num) {
+	this->id = (4 * num) + suit;
+	this->suit = suit;
+	this->num = num;
+	this->suit_str = suit_name[suit];
+}
+
+card::~card() {
+
+}
+
+/**/
+int main() {
+  card* c = new card(25);
+
+  println(c->id);
+  println(c->suit);
+  println(c->suit_str);
+  println(c->num);
+
+
+  return 0;
+}
---
+++ cpp/card/card.h
@@ -0,0 +1,28 @@
+#include "../common/common.h"
+
+//SUIT
+const int CLUB = 0;
+const int DAIA = 1;
+const int HEART = 2;
+const int SPADE = 3;
+
+//SUIT_NAME
+std::string suit_name[] = {
+	"CLUB",
+	"DAIA",
+	"HEART",
+	"SPADE"
+};
+
+class card {
+public:
+  int id;
+  int num;
+  int suit;
+  std::string suit_str;
+
+  card();
+  card(int id);
+  card(int suit, int num);
+  ~card();
+};
