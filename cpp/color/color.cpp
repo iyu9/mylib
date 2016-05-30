3654781b499cdf3e8722e63878a222cafb701aae
Tomii Yuto <tomii.yuto@gmail.com>
2016-05-30 23:26:43 +0900

    color fix

--- cpp/color/color.cpp
+++ cpp/color/color.cpp
@@ -1,11 +1,16 @@
 #include "color.h"
 
 color::color(float r, float g, float b, float a) {
-        
+	this->r = r;
+	this->g = g;
+	this->b = b;
+	this->a = a;        
 }
 
 color::color(float r, float g, float b) {
-        
+	this->r = r;
+	this->g = g;
+	this->b = b;        
 }
 
 color::color(){}
