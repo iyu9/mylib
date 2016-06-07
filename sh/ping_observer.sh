5ddaf571c83a0972b58d666a9bd304d89f613a02
tomii yuto <tomii@raspberrypi>
2016-06-07 23:11:06 +0900

    ping ovserve test

--- sh/ping_observer.sh
+++ sh/ping_observer.sh
@@ -3,7 +3,17 @@
 if [ $# -eq 1 ];
 
 then
-	ping $1
+	ping -c 10 $1;
+
+	if [$? -eq 0];
+			then
+			#OK 
+			echo "working"
+
+	else
+			#NG: mail etc
+			echo "not working"
+	fi
 else
 	echo "arg error"
 fi
