c8f7d5ea5857cfc685b76b0d1f162d5349c8218a
tomii yuto <tomii@raspberrypi>
2016-06-07 21:15:52 +0900

    add http

--- py/http/http.py
+++ py/http/http.py
@@ -2,12 +2,29 @@
 
 import urllib
 
-params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+#class sample
+#method needs self always
+class MyURL:
+	f = None
+	url = None
+	html = None
 
+	def __init__(self, url_):
+		url = url_
+		self.f = urllib.urlopen(url)
+
+	def get(self):
+		html = self.f.read()
+		print html
+
+m = MyURL("http://google.co.jp")
+m.get()
+
+###
+#params = urllib.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
 #GET
 #f = urllib.urlopen("http://google.co.jp%s" % params)
 #POST
 #f = urllib.urlopen("http://google.co.jp", params)
-
-f = urllib.urlopen("http://google.co.jp")
-print f.read()
+#f = urllib.urlopen("http://google.co.jp")
+#print f.read()
