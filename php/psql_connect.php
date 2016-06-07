c19feba4e6445a7ed36bf7dd5f6a77951270202a
tomii <tomii@localhost.localdomain>
2016-06-07 16:40:01 +0900

    php psql add

---
+++ php/psql_connect.php
@@ -0,0 +1,15 @@
+<?php
+  $db_conn = pg_connect("dbname=template1 user=tomii");
+
+  $query = 'SELECT * FROM test';
+  $result = pg_query($query);
+
+  while($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+  {
+    foreach($line as $col_value) {
+      echo $col_value;
+    }
+  }
+  pg_free_result($result);
+  pg_close($db_conn);
+?>
