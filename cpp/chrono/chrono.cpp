8aab3b99cbaf0058a5f14a10fac77eb5f5b20036
tomii yuto <tomii@raspberrypi>
2016-09-03 18:00:03 +0900

    update chrono

--- cpp/chrono/chrono.cpp
+++ cpp/chrono/chrono.cpp
@@ -1,27 +1,80 @@
 #include "chrono.h"
 
-chrono::chrono() {
-	prev_time = 0;
+chrono::chrono()
+{
+    start = clock();
+	prev_clock = start;
 }
 
-chrono::~chrono() {
+clock_t chrono::get_clock_sec()
+{
+	clock_t current = clock();
+	prev_clock = current;
 
+	return current;
 }
-
-double chrono::get_sec() {
-	double sec = (double)clock() / (double)CLOCKS_PER_SEC;
-	prev_time = sec;
+double chrono::get_sec()
+{
+	clock_t current = clock();
+	double sec = (double)current / CLOCKS_PER_SEC;
+	prev_clock = current;
 
 	return sec;
 }
 
-double chrono::get_delta() {
+clock_t chrono::get_clock_delta()
+{
 	clock_t current_clock = clock();
 	clock_t delta_clock = current_clock - prev_clock;
-	double delta_sec = (double) delta_clock / (double)CLOCKS_PER_SEC;
+
+	prev_clock = current_clock;
+	return delta_clock;
+}
+double chrono::get_delta()
+{
+	clock_t current_clock = clock();
+	clock_t delta_clock = current_clock - prev_clock;
+	double delta_sec = (double) delta_clock / CLOCKS_PER_SEC;
 
 	prev_clock = current_clock;
 	return delta_sec;
+}
+
+clock_t chrono::get_clock_time()
+{
+	clock_t current = clock();
+	return (current - start);
+}
+double chrono::get_time()
+{
+	clock_t current = clock();
+	double time = (double) (current - start) / CLOCKS_PER_SEC;
+
+	return time;
+}
+
+#include <stdio.h>
+#include <time.h>
+
+int main(void)
+{
+  chrono *c = new chrono();
+
+  clock_t start, end;
+  int i;
+
+  start = clock();
+  printf( "開始時間:%d¥n", start );
+
+  /* 何かの処理 */
+  for( i=0; i<500000000; i++ );
+
+  //end = clock();
+  //printf( "終了時間:%f¥n", (double) end / CLOCKS_PER_SEC  );
+  //printf( "処理時間:%f¥n", (double) (end - start) / CLOCKS_PER_SEC );
+
+  printf("start from time = %f", c->get_time());
+  return 0;
 }
 
 /*
--- cpp/chrono/chrono.h
+++ cpp/chrono/chrono.h
@@ -2,13 +2,19 @@
 
 class chrono {
 private:
-  double prev_time;
+  clock_t start;
+  clock_t end;
   clock_t prev_clock;
 
 public:
   chrono();
   ~chrono();
 
+  clock_t get_clock_time();
+  clock_t get_clock_sec();
+  clock_t get_clock_delta();
+
+  double get_time();
   double get_sec();
   double get_delta();
 };
