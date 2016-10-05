005555386c28030758baa997e5ac584f574be32c
iyu9 <tomii.yuto@gmail.com>
2016-10-05 14:12:02 +0900

    updated with autoupdate.sh

---
+++ py/mail/test_mailsend.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*- 
+
+import smtplib
+from email.MIMEText import MIMEText
+from email.Header import Header
+from email.Utils import formatdate
+
+from_address = 'FROM_ADDR'
+to_address   = 'DEST_ADDR'
+
+charset = 'ISO-2022-JP'
+subject = u'メールの件名です'
+text    = u'メールの本文です'
+
+msg = MIMEText(text.encode(charset), 'plain', charset)
+msg['Subject'] = Header(subject, charset)
+msg['From']    = from_address
+msg['To']      = to_address
+msg['Date']    = formatdate(localtime=True)
+
+smtp = smtplib.SMTP()
+smtp.sendmail(from_address, to_address, msg.as_string())
+smtp.close()
