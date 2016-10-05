ab617802976a50a570f1bcd1f9c1edd9c0c93ced
iyu9 <tomii.yuto@gmail.com>
2016-10-05 10:31:22 +0900

    updated with autoupdate.sh

--- sh/autopush.sh
+++ sh/autopush.sh
@@ -1,4 +1,12 @@
-git add *
-git commit -m "updated all"
-git push
+#!/bin/sh
 
+git add *
+
+if [$# -eq 1];
+  then
+	git commit -m $1;
+  else
+	git commit -m "updated with autoupdate.sh";
+fi
+
+git push
