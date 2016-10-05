f1e0e28320ab85b5b96a823734a5489a71475840
iyu9 <tomii.yuto@gmail.com>
2016-10-05 15:24:56 +0900

    update sql

--- sql/create_m_user.sql
+++ sql/create_m_user.sql
@@ -1,3 +1,7 @@
+DROP TABLE m_user;
+DROP TABLE t_address;
+DROP TABLE t_date;
+
 CREATE TABLE m_user(id int, name text);
 CREATE TABLE t_address(id int, address text);
 CREATE TABLE t_date(id int, login date);
