f98d83c304fa25b48ceb134189e4f98a3d819dfc
iyu9 <tomii.yuto@gmail.com>
2016-10-21 11:11:20 +0900

    updated with autoupdate.sh

--- cpp/util/util.h
+++
@@ -1,8 +0,0 @@
-
-static float lerp(float normal, float x0, float x, float y, float y0) {
-
-}
-
-static float defun() {
-  
-}
---
+++ cpp/util/util.hpp
@@ -0,0 +1,47 @@
+#include "../common/common.h"
+
+/*
+ * 境界内の値を得る
+ */
+float cut(float val, float lower, float upper)
+{
+    if (val < lower)
+    {
+        return lower;
+    }
+
+    if (val > upper)
+    {
+        return upper;
+    }
+
+    return val;
+}
+
+/*
+ * 値の境界判定
+ */
+bool is_between(float val, float lower, float upper)
+{
+    if (val < lower)
+    {
+        return false;
+    }
+
+    if (val > upper)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+/**/
+int main()
+{ 
+    int cut_test = cut(180, 170, 190);
+    bool is_between_test = is_between(180, 170, 190);
+ 
+    return 0;
+}
+/**/
