84a9c20aae9edec959a14c83996a80dbc4b112cd
iyu9 <tomii.yuto@gmail.com>
2016-10-23 13:16:52 +0900

    add camera

---
+++ cpp/camera/camera.cpp
@@ -0,0 +1,33 @@
+class Camera
+{
+  public:
+    float waitTime = 0f;
+    
+    float amp = 1f;
+    float freq = 20f;
+    float sec = 1.5f;
+
+    /*
+     * カメラの振動を開始します。
+     * sec_ : 振動時間
+     * amp_ : 振幅
+     * freq_: 振動数
+     */
+    public void StartShakeCamera(float sec_, float amp_, float freq_)
+    {
+        sec = sec_;
+        amp = amp_;
+        freq = freq_;   
+    }
+    
+    public void Update()
+    {
+        if (waitTime > 0f)
+        {
+            waitTime -= 0.000001f;       
+        }else
+        {
+            waitTime = 0f;        
+        }        
+    }     
+};
