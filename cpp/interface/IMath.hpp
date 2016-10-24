190b9d484e3e2023651ee3b041439e63ec11d0b4
tomii yuto <tomii@raspberrypi>
2016-10-24 23:54:12 +0900

    updated with autoupdate.sh

--- cpp/interface/IMath.hpp
+++ cpp/interface/IMath.hpp
@@ -1,6 +1,21 @@
+/*
+ * Mathematics Interface class
+ */
 class IMath
 {
   public:
+	const float PI = 3.14159265;
+	const float e = 2.71828;
+	const float g = 9.8;
+
     double Sin(double x);      
     double Cos(double x);
+	double Tan(double x);
+
+	double ArcSin(double x);
+	double ArcCos(double x);
+	double ArcTan(double x);
+
+	double RadianToEuler(double);
+	double EulerToRadian(double);
 };
