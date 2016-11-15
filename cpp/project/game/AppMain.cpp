d458c6d662efa83faff7f2cec9cc0ebba9f3adc9
tomii <tomii@localhost.localdomain>
2016-11-15 15:10:59 +0900

    updated with autoupdate.sh

--- cpp/project/game/AppMain.cpp
+++ cpp/project/game/AppMain.cpp
@@ -1,3 +1,4 @@
+#include "../../common_class/common/common.h"
 
 /*
  * Application Entry Point
@@ -5,15 +6,30 @@
 namespace AppMain
 {
   bool isQuit = false;
+  int currentScene = 0;
+  std::string cmd;
+
+  enum Scene
+  {
+	Loading,
+	Title,
+	Start,
+	Continue,
+	Option,
+	Exit
+  };
 
   bool Init()
   {
+	currentScene = Loading;
 	return true;
   }
 
   void MainLoop()
   {
-	
+	std::cin >>	cmd;
+	std::cout << std::endl << "inputed: ";
+	std::cout << cmd << std::endl;
   }
 }
 
