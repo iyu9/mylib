33c2e001acebdaa40e7f029083745e2c52828d49
tomii <tomii@localhost.localdomain>
2016-11-17 19:04:19 +0900

    updated with autoupdate.sh

--- cpp/project/game/graphics/GLManager.hpp
+++ cpp/project/game/graphics/GLManager.hpp
@@ -5,20 +5,24 @@ namespace GLManager
 {
   class GLManager
   {
-	void Init()
-	{
-	  //
-	}
+	public:
+	  void Init()
+	  {
+		glutInit(0, NULL);
+		glutClearWindow(NULL);
+		glutDisplayFunc(render);
+		glutMainLoop();
+	  }
 
-	void Render()
-	{
-	  glClear(GL_COLOR_BUFFER_BIT);
-	  glBegin(GL_LINE_LOOP);
-	  glVertex2d(0,0);
-	  glVertex2d(1,0);
-	  glVertex2d(1,1);
-	  glVertex2d(0,1);
-	  glFlush();
-	} 
+	  void Render()
+	  {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_LINE_LOOP);
+		  glVertex2d(0,0);
+		  glVertex2d(1,0);
+		  glVertex2d(1,1);
+		  glVertex2d(0,1);
+		glFlush();
+	  } 
   };
 }
