5fe6474e61636414bee0fd9d54571598e359744a
iyu9 <tomii.yuto@gmail.com>
2016-12-05 10:49:22 +0900

    Update sample_t_test.R

--- R/Testing/sample_t_test.R
+++ R/Testing/sample_t_test.R
@@ -1,4 +1,7 @@
-# T-TESTING
 a <- c(70, 67, 81, 92, 78, 62, 85, 73)
 b <- c(60, 75, 48, 58, 80, 57, 50)
+
+# one-sample t-testing
+t.test(a)
+# two-sample t-testing
 t.test(x=a, y=b, conf.level=0.95, var.equal=TRUE)
