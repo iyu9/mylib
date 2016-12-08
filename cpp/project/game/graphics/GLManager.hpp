6b40f0aef1dab46510180f161961dc217800d4fb
iyu9 <tomii.yuto@gmail.com>
2016-12-08 14:46:23 +0900

    Update_GLManager

--- cpp/project/game/graphics/GLManager.hpp
+++ cpp/project/game/graphics/GLManager.hpp
@@ -6,6 +6,55 @@
  * OpenGL Graphics Control Class
  * Referenced by https://tokoik.github.io/opengl/libglut.html#3.1
  */
+
+#define TEX_HEIGHT 16 
+#define TEX_WIDTH 16 
+
+static GLubyte image[TEX_HEIGHT][TEX_WIDTH][4];
+
+void InitTexture(void) 
+{
+  int i, j, c;
+  for (i = 0; i < TEX_HEIGHT; i++) 
+  {
+    for (j = 0; j < TEX_WIDTH; j++)
+    {
+      c = ( ((i&0x01)==0)^((j&0x01)==0) );
+      image[i][j][0] = image[i][j][1] = image[i][j][2] = c*255;
+      image[i][j][3]=255;
+    }
+  }
+} 
+
+void displayTexPolygon(void)
+{
+  glEnable(GL_TEXTURE_2D);
+  glBegin(GL_QUADS);
+	glTexCoord2f(0.0, 0.0); glVertex3f(-5.0,-5.0, 0.0);
+	glTexCoord2f(0.0, 1.0); glVertex3f(-5.0, 5.0, 0.0);
+	glTexCoord2f(1.0, 1.0); glVertex3f( 5.0, 5.0, 0.0);
+	glTexCoord2f(1.0, 0.0); glVertex3f( 5.0,-5.0, 0.0);
+  glEnd();
+  glDisable(GL_TEXTURE_2D);
+} 
+
+void displayAltanative()
+{
+  static float spin = 0.0;
+  glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+  glPushMatrix();
+  {
+	glTranslatef(0.0, 0.0, -20.0);
+	glRotatef(spin, 0.0, 1.0, 0.0);
+	glColor3f(1,1,1);
+	displayTexPolygon();
+  }
+  spin++;
+  glPopMatrix();
+  glFlush();
+  glutSwapBuffers();
+}
+
 namespace GL
 {
   struct Vec2
@@ -39,9 +88,9 @@ namespace GL
 
   enum RenderType
   {
-	Line,
-	Rect,
-	Poly,
+    Line,
+    Rect,
+    Poly,
   };
 
   class Object
@@ -58,12 +107,12 @@ namespace GL
 
 	  int GetID()
 	  {
-		return id;
+      return id;
 	  }
 
 	  int GetRenderType()
 	  {
-		return renderType;
+      return renderType;
 	  }
   };
 
@@ -76,12 +125,12 @@ namespace GL
 
 	  void Save(const char* fileName)
 	  {
-		//Save Scene File
+      //Save Scene File
 	  }
 
 	  void Load(const char* sceneName)
 	  {
-		//Load Scene File	
+      //Load Scene File	
 	  }
 
 	  //read only pointer ?
@@ -94,7 +143,7 @@ namespace GL
   const Vec2 WindowPos = {100, 100};
   const Vec2 WindowSize = {320, 240};
 
-  const int IntervalUpdate = 100; //msec
+  const int IntervalUpdate = 10; //msec
 
   static Vec2 pos;
   const float MoveVal = 0.1;
@@ -126,7 +175,7 @@ namespace GL
 
       static void DrawText()
       {
-		//add plugin...  
+        //add plugin...  
       }
 
       //for Auto Handling//
@@ -139,9 +188,17 @@ namespace GL
 
       static void Reshape(int w, int h)
       {
+		/*
         glViewport(0, 0, w, h);  
         glLoadIdentity();
         glOrtho(-w / 200.0, w / 200.0, -h / 200.0, h / 200.0, -1.0, 1.0);
+		*/
+		glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glFrustum(-5.0, 5.0, -5.0, 5.0, 5.0, 500.0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
       }
 
       static void Timer(int value)
@@ -205,20 +262,37 @@ namespace GL
 
       static void Init(int* argc, char* argv[])
       {
+        //Create Window
         glutInitWindowPosition(WindowPos.x, WindowPos.y);
         glutInitWindowSize(WindowSize.x, WindowSize.y);
         glutInit(argc, argv);
-        glutInitDisplayMode(GLUT_RGBA);
+        glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
         glutCreateWindow(argv[0]);
 
-        //Set Auto Execute Functions
-        glutDisplayFunc(Display);
+		//Set Depth Setting
+		glClearColor(0.0, 0.0, 0.0, 0.0);
+		glDepthFunc(GL_LEQUAL);
+		glEnable(GL_DEPTH_TEST);
+
+        //Set Texture Setting
+        InitTexture();
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, TEX_WIDTH, TEX_HEIGHT, 
+          0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+        //Set Auto-Processing Functions
+        //glutDisplayFunc(Display);
+        glutDisplayFunc(displayAltanative);
         glutReshapeFunc(Reshape);
         glutMouseFunc(Mouse);
         glutKeyboardFunc(Keyboard);
         glutTimerFunc(IntervalUpdate, Timer, 0);
 
-        glClearColor(0,0,0,1);
+        glClearColor(0, 0, 0, 1);
         glutMainLoop();
       }
 	  
