6765f4e70e226b0245fad25eed1d6b0a7e15981a
iyu9 <tomii.yuto@gmail.com>
2017-03-08 10:53:13 +0900

    update http_common.py

--- py/server/http_common.py
+++ py/server/http_common.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-ContentTypeHTML = 'Content-Type: text/html'
+ContentTypeHTML = 'Content-type: text/html\n\n'
+
 print(ContentTypeHTML)
+print("<html>test</html>")
