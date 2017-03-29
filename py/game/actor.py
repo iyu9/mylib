acb02748cbc135dc9ed4a349c8f2453dc87ffa22
iyu9 <tomii.yuto@gmail.com>
2017-03-29 09:48:12 +0900

    actor.py

--- py/game/actor.py
+++ py/game/actor.py
@@ -1,27 +1,57 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 class Actor:
 
   def __init__(self):
 	self.id = 1
 	self.name = "hoge"
+	self.lv = 1
 	self.hp = 10
+	self.mp = 10
+
 	self.atk = 2
 	self.defence = 1
+	self.spd = 1
+	self.luk = 1
 
-	self.x = self.y = 0
+	self.x = 0
+	self.y = 0
 	self.stat = 0
 	self.is_dead = 0
 
-  def Log(self):
-	print(str(self.id) + ", " + self.name)
+  def ShuffleStatus(self):
+	self.hp      = random.randint(10, 20)
+	slef.lv      = 1
+	self.atk     = random.randint(5, 8) 	
+	self.defence = random.randint(5, 8) 	
+	self.spd     = random.randint(5, 8)
+	self.luk     = random.randint(5, 8)
 
-  def Attack(self, opt):
-	opt.hp -= self.atk - opt.defence
-	print("optHP: " + str(opt.hp))
+  def PrintStatus(self):
+	print(self.name + " Status")
+	print("name => " + str(self.name))
+	print("hp => " + str(self.hp))
+	print("atk => "  + str(self.atk))
+	print("defence => " + str(self.defence))
+
+  def Attack(self, target):
+	damage = self.atk - target.defence
+	target.hp -= damage
+	print(self.name + " Attacked => " + target.name)
+	print(target.name + " Damaged => " + str(damage))
 
   def Move(self, x_, y_):
 	self.x = x_
 	self.y = y_
+	print(self.name + " Moved =>" 
+	+ " (" + str(self.x)  + ", " + str(self.y) + ")")
 
-player = Actor()
-enemy = Actor()
-player.Attack(enemy)
+if __name__ == '__main__':
+  player = Actor()
+  enemy  = Actor()
+  player.Attack(enemy)
+  player.Move(2, 2)
+  player.PrintStatus()
+  enemy.PrintStatus()
+
