db40c6c5bbd7c3bb3ab6d042bd2123d8a5e911a4
iyu9 <tomii.yuto@gmail.com>
2017-04-14 09:29:12 +0900

    timer.py

--- py/game/timer.py
+++ py/game/timer.py
@@ -6,56 +6,56 @@ import time
 class Timer:
   is_update_frame = False
 
-  startSeconds = 0
-  lastSeconds = 0
+  start_seconds = 0
+  last_seconds = 0
 
-  timerStartSeconds = 0
-  timerFinishSeconds = 0
+  start_timer = 0
+  finish_timer = 0
 
   def __init__(self):
-	self.startSeconds = self.getSeconds()
-	self.lastSeconds = self.startSeconds
+	self.start_seconds = self.get_seconds()
+	self.last_seconds = self.start_seconds
 
-  def getSeconds(self):
+  def get_seconds(self):
 	return time.clock()
 
-  def getMinutes(self):
+  def get_minutes(self):
 	return time.clock() / (60.0)
 
-  def getHours(self):
+  def get_hours(self):
 	return time.clock() / (60.0 * 60.0)
 
-  def getDays(self):
+  def get_days(self):
 	return time.clock() / (24.0 * 60.0 * 60.0)
 
-  def getDelta(self):
-	res = self.getSeconds() - self.lastSeconds
+  def get_delta(self):
+	res = self.get_seconds() - self.last_seconds
 
 	if self.is_update_frame == False:
-	  # for check frame loop check
+	  # for check frame loop
 	  self.is_update_frame = True
-	  self.lastSeconds = self.getSeconds()
+	  self.last_seconds = self.get_seconds()
 	return res
 
-  def start(self, finishSeconds):
-	self.startTimer = self.getSeconds()
-	self.finishTimer = finishSeconds
+  def start(self, finish_seconds):
+	self.start_timer = self.get_seconds()
+	self.finish_timer = finish_seconds
 
   def stop(self):
-	self.finishTimer = 0
+	self.finish_timer = 0
 
   def is_finish(self):
-	return self.getSeconds() >= self.finishTimer
+	return self.get_seconds() >= self.finish_timer
 
 if __name__ == "__main__":
   timer = Timer()
 
-  print str(timer.getSeconds()) + " secs"
-  print str(timer.getMinutes()) + " mins"
-  print str(timer.getHours())   + " hours"
-  print str(timer.getDays())    + " days"
-  print str(timer.getDelta())   + " from Prev Frame"
+  print str(timer.get_seconds()) + " secs"
+  print str(timer.get_minutes()) + " mins"
+  print str(timer.get_hours())   + " hours"
+  print str(timer.get_days())    + " days"
+  print str(timer.get_delta())   + " from Prev Frame"
 
   timer.start(1.0)
   while timer.is_finish() == False:
-	print timer.getSeconds()
+	print timer.get_seconds()
