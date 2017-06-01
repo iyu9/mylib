719885f9d432944b794fbe43a8173c6f32c5cb43
iyu9 <tomii.yuto@gmail.com>
2017-06-01 18:50:31 +0900

    pyglet

--- py/pyglet/render_image.py
+++ py/pyglet/render_image.py
@@ -1,11 +1,25 @@
 import pyglet
 
+x = 0
+y = 0
+
+# for Initializeing
 window = pyglet.window.Window()
-image = pyglet.image.load('../cocos2d/sample.png')
+image  = pyglet.image.load('../cocos2d/sample.png')
 sprite = pyglet.sprite.Sprite(image)
 
 @window.event
 def on_draw():
-  sprite.draw()
+  image.blit(x, y)
+  # sprite.draw()
+
+@window.event
+def on_key_press():
+  x+=1
+  pass
+
+@window.event
+def on_key_release():
+  pass
 
 pyglet.app.run()
