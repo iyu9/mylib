e1459a512e9dee49a2ed76b266f0bc4e8c38a429
iyu9 <tomii.yuto@gmail.com>
2017-06-20 10:44:48 +0900

    vector.py

--- py/game/vector.py
+++ py/game/vector.py
@@ -50,8 +50,43 @@ class Vector3:
 	self.y /= v.y
 	self.z /= v.z
 
+class Vector4:
+
+  def __init__(self):
+	self.x = 0
+	self.y = 0
+	self.z = 0
+	self.w = 0
+
+  def add(self, v):
+	self.x += v.x
+	self.y += v.y
+	self.z += v.z
+	self.w += v.w
+
+  def minus(self, v):
+	self.x -= v.x
+	self.y -= v.y
+	self.z -= v.z
+	self.w -= v.w
+
+  def times(self, v):
+	self.x *= v.x
+	self.y *= v.y
+	self.z *= v.z
+	self.w *= v.w
+
+  def divs(self, v):
+	self.x /= v.x
+	self.y /= v.y
+	self.z /= v.z
+	self.w /= v.w
+
 if __name__ == '__main__':
   v2 = Vector2()
   v3 = Vector3()
+  v4 = Vector4()
+
   print v2.x, v2.y
   print v3.x, v3.y, v3.z
+  print v4.x, v4.y, v4.z, v4.w
