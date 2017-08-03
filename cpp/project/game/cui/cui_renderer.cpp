183ca15cd9f4d04cf2f7e7a61665614a19cf2f73
iyu9 <tomii.yuto@gmail.com>
2017-08-03 12:33:35 +0900

    update

--- cpp/project/game/cui/cui_renderer.cpp
+++ cpp/project/game/cui/cui_renderer.cpp
@@ -8,9 +8,22 @@
 
 using namespace std;
 
-const double UPDATE_FPS = 0.033;
+//--------------------------
+// Game variables
+//--------------------------
 
-char buffer[SCREEN_WIDTH][SCREEN_HEIGHT];
+int game_mode = 0;
+const int Title  = 0;
+const int Option = 1;
+const int Main   = 2;
+const int Result = 3;
+
+//--------------------------
+// CUI Screen Setting
+//--------------------------
+const double UPDATE_FPS = 0.1;
+
+char buf[SCREEN_WIDTH][SCREEN_HEIGHT];
 double timer_secs = 0;
 clock_t start;
 long frame = 0;
@@ -59,24 +72,55 @@ void render(char data[SCREEN_WIDTH][SCREEN_HEIGHT])
   } 
 }
 
-void clear_buffer()
+void clear_buf()
 {
   for (int x = 0; x < SCREEN_WIDTH; x++)
   {
 	for (int y = 0; y < SCREEN_HEIGHT; y++)
 	{
-	  buffer[x][y] = ' ';
+	  buf[x][y] = ' ';
 	}
   } 
 }
 
-void draw_rect(int x1, int y1, int x2, int y2, char ch)
+void draw_rect(int start_x, int start_y, int end_x, int end_y, char ch)
 {
-  for (int x = x1; x < x2; x++)
+  for (int x = start_x; x < end_x; x++)
   {
-	for (int y = y1; y < y2; y++)
+	for (int y = start_y; y < end_y; y++)
 	{
-	  buffer[x][y] = ch;
+	  buf[x][y] = ch;
+	}
+  }  
+}
+void draw_rect(Vec2 start_pos, Vec2 end_pos, char ch)
+{
+  for (int x = start_pos.x; x < end_pos.x; x++)
+  {
+	for (int y = start_pos.y; y < end_pos.y; y++)
+	{
+	  buf[x][y] = ch;
+	}
+  }  
+}
+
+void draw_rectsize(int start_x, int start_y, int size_x, int size_y, char ch)
+{
+  for (int x = start_x; x < start_x + size_x; x++)
+  {
+	for (int y = start_y; y < start_y + size_y; y++)
+	{
+	  buf[x][y] = ch;
+	}
+  }  
+}
+void draw_rectsize(Vec2 pos, Vec2 size, char ch)
+{
+  for (int x = pos.x; x < pos.x + size.x; x++)
+  {
+	for (int y = pos.y; y < pos.y + size.y; y++)
+	{
+	  buf[x][y] = ch;
 	}
   }  
 }
@@ -91,7 +135,7 @@ void draw_line(int x0, int y0, int x1, int y1, char ch)
   int y = y0;
   for (int x = x0; x <= x1; x++)
   {
-	buffer[x][y] = ch;
+	buf[x][y] = ch;
 	err = err + d_err;
 	if (err >= 0.5)
 	{
@@ -105,7 +149,7 @@ void draw_point(int x, int y, char ch)
 {
   if (x <= SCREEN_WIDTH && y <= SCREEN_HEIGHT)
   {
-	buffer[x][y] = ch;
+	buf[x][y] = ch;
   }
 }
 
@@ -135,38 +179,60 @@ void draw_character(int x, int y, char ch)
 	  draw_point(3, 4, ch);
 	  break;
 
+	case 't':
+	  //T
+	  draw_rect(0, 0, 9, 5, '*');
+	  draw_rect(3, 2, 6, 10, '*');
+
+	  //O
+	  draw_rect(10, 0,  20,  3, '*');
+	  draw_rect(17, 0,  20, 10, '*');
+	  draw_rect(10, 8,  20, 10, '*');
+	  draw_rect(10, 0,  13, 10, '*');
+	  break;
+
 	default:
 	  draw_point(x, y, ch);
 	  break;
   }
 }
 
-void store_buffer()
+/*
+** App Main Procedure
+*/
+void update()
 {
-  draw_rect(0, 0, frame % SCREEN_WIDTH, frame, '@');
- 
-  //SNOW
-  //for (int i = 0; i < SCREEN_WIDTH; i++)
-  //{
-  //	draw_point(i, (frame + 4 * i) % SCREEN_HEIGHT, '*');
-  //}
+  switch(game_mode)
+  {
+	case Title:
+	  draw_character(0, 0, 't');
+	  //draw_rect(0, 0, frame % SCREEN_WIDTH, frame, '@');
+
+	  if (frame > 20)
+	  {
+		game_mode = Main;
+	  }
+	  break;
+
+	case Main:
+	  //SNOW
+	  for (int i = 0; i < SCREEN_WIDTH; i++)
+	  {
+	  	draw_point(i, (frame + 4 * i) % SCREEN_HEIGHT, '*');
+	  }
+	  break;
+  }
 }
 
 void clear()
 {
-  clear_buffer();
+  clear_buf();
   system("clear");  
 }
 
-//--------------------------
-//Game Proc
-//--------------------------
 
-int mode = 0;
-const int Title  = 0;
-const int Main   = 1;
-const int Result = 2;
-
+//--------------------------
+// For UnitTesting
 //--------------------------
 
 #ifndef DEBUG
@@ -183,8 +249,8 @@ int main()
 	  frame++;
 
 	  clear();
-	  store_buffer();
-	  render(buffer);
+	  update();
+	  render(buf);
 	}
   }
   return 0;  
