3a446d2e186eb0068885bbefddc27f988eb05ceb
iyu9 <tomii.yuto@gmail.com>
2017-08-23 16:30:42 +0900

    add_rwbmp

---
+++ cpp/common/rwbmp/main.c
@@ -0,0 +1,24 @@
+#include "bitmap.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	Image *colorimg;
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage: program <inputfile> <outputfile>\n");
+		exit(1);
+	}
+
+	if ((colorimg = Read_Bmp(argv[1])) == NULL) {
+		exit(1);
+	}
+
+	if (Write_Bmp(argv[2], colorimg)) {
+		exit(1);
+	}
+
+	Free_Image(colorimg);
+	return 0;
+}
