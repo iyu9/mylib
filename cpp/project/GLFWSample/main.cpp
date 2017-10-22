b8be2a906091fd0b6ab99746f66bc61f5a867725
Tomii Yuto <yuto@tomii-mac.local>
2017-10-22 23:50:40 +0900

    updated

--- cpp/project/GLFWSample/main.cpp
+++ cpp/project/GLFWSample/main.cpp
@@ -1,11 +1,29 @@
 #include <GLFW/glfw3.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+static void error_callback(int error, const char* description)
+{
+  fputs(description, stderr);
+}
+
+static void key_callback(GLFWwindow* window, int key, int scanmode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+  {
+	glfwSetWindowShouldClose(window, GL_TRUE);
+  }
+}
 
 int main()
 {
   if (!glfwInit())
+  {
     return 1;
+  }
 
-  GLFWwindow *const window = glfwCreateWindow(640, 480, "SAMPLE", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "SAMPLE", NULL, NULL);
+
   if (!window)
   {
     glfwTerminate();
@@ -13,13 +31,12 @@ int main()
   }
 
   glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key_callback);
 
   while (!glfwWindowShouldClose(window))
   {
     glClear(GL_COLOR_BUFFER_BIT);
-
     glfwSwapBuffers(window);
-
     glfwPollEvents();
   }
 
