41bd71f4e6bf09aeb7d83976a7ef70bcf2d796ca
iyu9 <tomii.yuto@gmail.com>
2017-10-25 18:18:16 +0900

    Create GLVec.h

---
+++ cpp/project/GLFWSample/GLVec.h
@@ -0,0 +1,25 @@
+struct GLPoint
+{
+  double x;
+};
+
+struct GLVec2
+{
+  double x;
+  double y;
+};
+
+struct GLVec3
+{
+  double x;
+  double y;
+  double z;
+};
+
+struct GLVec4
+{
+  double x;
+  double y;
+  double z;
+  double w;
+};
