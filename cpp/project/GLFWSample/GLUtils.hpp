8bb60779cfb2be642ea63a0f21f22649538a1f90
iyu9 <tomii.yuto@gmail.com>
2017-11-01 14:20:00 +0900

    Update GLUtils.hpp

--- cpp/project/GLFWSample/GLUtils.hpp
+++ cpp/project/GLFWSample/GLUtils.hpp
@@ -1,5 +1,33 @@
 namespace GLUtils
 {
+  #define TEST_TEX_HEIGHT 16
+  #define TEST_TEX_WIDTH 16
+
+  GLubyte image[TEST_TEX_HEIGHT][TEST_TEX_WIDTH][4];
+
+  static void CheckRenderTexture()
+  {
+    for (int i = 0; i < TEST_TEX_HEIGHT; i++)
+    {
+      for (int j = 0; j < TEST_TEX_WIDTH; j++)
+      {
+        int color = ((i & 0x01) == 0) ^ ((j & 0x01) == 0);
+        image[i][j][0] = color * 255;
+        image[i][j][1] = color * 255;
+        image[i][j][2] = color * 255;
+        image[i][j][3] = 255;
+      }
+    }
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, TEST_TEX_WIDTH, TEST_TEX_HEIGHT,
+      0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+  }
+
   static void DrawTexture(float x, float y, float w, float h)
   {
     glEnable(GL_TEXTURE_2D);
