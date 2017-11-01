ac87e0b549ba6e3636cd6acfd0d0ffe3a2dd45f1
iyu9 <tomii.yuto@gmail.com>
2017-11-01 14:18:07 +0900

    Update main.cpp

--- cpp/project/GLFWSample/main.cpp
+++ cpp/project/GLFWSample/main.cpp
@@ -1,23 +1,37 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "GLVec.hpp"
-#include "GLUtils.hpp"
-#include "GLBuiltInCallback.hpp"
+#include "GLVec.h"
+#include "GLSL.h"
+#include "GLUtils.h"
+#include "GLBuiltInCallback.h"
+#include "GLBmpLoader.hpp"
+
+const int g_windowWidth = 640;
+const int g_windowHeight = 480;
 
 GLVec2 pos;
+BMP *bmp;
 
-static void MainLoop()
+void CustomInit()
+{
+  GLUtils::CheckRenderTexture();
+
+  bmp = new BMP("sample.bmp");
+  glBindTexture(GL_TEXTURE_2D, bmp->texture);
+}
+
+void MainLoop()
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
   //2dRect
-  glColor3f(1, 0, 0);
-  GLUtils::DrawRect(pos.x, pos.y, 1.0f, 1.0f);
+  //glColor3f(0, 1, 0);
+  //GLUtils::DrawRect(pos.x, pos.y, 0.5f, 0.5f);
 
-  //2dLine
-  glColor3f(0, 1, 0);
-  GLUtils::DrawLine(pos.x, pos.y, 1.0f, 1.0f, 20);
+  //2dTexture
+  glColor3f(1, 1, 1);
+  GLUtils::DrawTexture(-0.8f + pos.x, -0.8f + pos.y, 1.6f, 1.6f);
 
   glFlush();
 }
@@ -31,7 +45,8 @@ int main()
     return 1;
   }
 
-  GLFWwindow *window = glfwCreateWindow(640, 480, "SAMPLE", NULL, NULL);
+  GLFWwindow *window =
+    glfwCreateWindow(g_windowWidth, g_windowHeight, "SAMPLE", NULL, NULL);
 
   if (!window)
   {
@@ -44,9 +59,12 @@ int main()
   glfwSetKeyCallback(window, key_callback);
   glfwSetCursorPosCallback(window, cursor_pos_callback);
 
+  CustomInit();
+
   while (!glfwWindowShouldClose(window))
   {
     MainLoop();
+
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
