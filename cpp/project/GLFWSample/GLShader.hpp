1038767a50cfe6aeff2480f19fe092762649e7f1
iyu9 <tomii.yuto@gmail.com>
2017-11-02 12:53:48 +0900

    Update GLShader.hpp

--- cpp/project/GLFWSample/GLShader.hpp
+++ cpp/project/GLFWSample/GLShader.hpp
@@ -1,33 +1,67 @@
-//Shader Utils
-
 const char *vertex_shader =
-  "in vec4 position;\n"
-  "out vec4 myPos;\n"
-  "void main() {\n"
-  "  myPos = position;\n"
-  "  gl_Position = position;\n"
-  "}\n";
+"void main(void){ \n"
+  "gl_Position = gl_ModelViewMatrix * gl_Vertex; \n"
+  "gl_FrontColor = vec4(1.0) - gl_Color; \n"
+"}";
 
 const char *fragment_shader =
-  "uniform float phase;\n"
-  "in vec4 myPos;\n"
-  "void main() {\n"
-  "  gl_FragColor = ...;\n"
-  "}\n";
+"void main(){ \n"
+"  gl_FragColor = vec4(1.0) - gl_Color; \n"
+"}";
+GLuint shader_program;
+
+//リンクする
+void Link(GLuint prog)
+{
+  GLsizei size, len;
+  GLint linked;
+  char *infoLog;
+  glLinkProgram(prog);
+  glGetProgramiv(prog, GL_LINK_STATUS, &linked);
+
+  if (linked == GL_FALSE)
+  {
+    printf("リンクできませんでした!! \n");
+    glGetProgramiv(prog, GL_INFO_LOG_LENGTH, &size);
+    if (size > 0)
+    {
+      infoLog = (char *)malloc(size);
+      glGetProgramInfoLog(prog, size, &len, infoLog);
+      printf(infoLog);
+      free(infoLog);
+    }
+  }
+}
 
 void CompileAndLinkShader()
 {
-  // Compile the vertex shader.
+  //glewの初期化
+  GLenum err = glewInit();
+  if (err != GLEW_OK)
+  {
+    printf("Error: %s\n", glewGetErrorString(err));
+  }
+
+  // GPU,OpenGL情報
+  printf("VENDOR= %s \n", glGetString(GL_VENDOR));
+  printf("GPU= %s \n", glGetString(GL_RENDERER));
+  printf("OpenGL= %s \n", glGetString(GL_VERSION));
+  printf("GLSL= %s \n", glGetString(GL_SHADING_LANGUAGE_VERSION));
+
   GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vshader, 1, &vertex_shader, 0);
-   
+
   // Compile the fragment shader.
   GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fshader, 1, &fragment_shader, 0);
-    
+
   // Create a program that stitches the two shader stages together.
-  GLuint shader_program = glCreateProgram();
+  shader_program = glCreateProgram();
   glAttachShader(shader_program, vshader);
   glAttachShader(shader_program, fshader);
-  glLinkProgram(shader_program);
+
+  glDeleteShader(vshader);
+  glDeleteShader(fshader);
+
+  Link(shader_program);
 }
