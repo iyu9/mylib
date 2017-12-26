d7d6c515bee865b87042d7948c6b38bd88d6cbbd
iyu9 <tomii.yuto@gmail.com>
2017-12-26 09:56:51 +0900

    del_shell_directory

--- sql/create_m_user.sql
+++ sql/create_m_user.sql
@@ -3,5 +3,6 @@ DROP TABLE t_address;
 DROP TABLE t_date;
 
 CREATE TABLE m_user(id int, name text);
+CREATE TABLE m_user_info(id int, info text);
 CREATE TABLE t_address(id int, address text);
 CREATE TABLE t_date(id int, login date);
