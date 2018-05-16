dde9a11348107dadf879f9b8a6f2b1a5f3c59288
iyu9 <tomii.yuto@gmail.com>
2018-05-16 13:20:55 +0900

    update_all

--- elisp/sample.el
+++ elisp/sample.el
@@ -1,2 +1,6 @@
-(message "hoge")
-(message "test")
+; emacs --script test.el
+(defun message-sample ()
+(let (list)
+(dotimes (i 10)
+(push i list)
+(message "%s" list))))
--- elisp/test.el
+++
@@ -1,6 +0,0 @@
-; emacs --script test.el
-(defun message-sample ()
-(let (list)
-(dotimes (i 10)
-(push i list)
-(message "%s" list))))
